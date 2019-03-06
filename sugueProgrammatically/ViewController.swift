039c4213e5e79ea23352b9dd66f95b29363418c0
Yury Lvov <yury.lvov@leverages.jp>
2019-03-06 11:48:49 +0900

    Initial Commit

---
+++ sugueProgrammatically/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sugueProgrammatically
+//
+//  Created by リヴォーフ　ユーリ on 2019/03/06.
+//  Copyright © 2019 リヴォーフ　ユーリ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
